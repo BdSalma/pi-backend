b2551c034ea9f419fdc9305fce323187d80b6ff1
Walehamdi1 <walahamdi0@gmail.com>
2024-02-27 21:41:42 +0100

    second

--- Templateexamen23-24/src/main/java/tn/examen/templateexamen2324/controller/ReclamationController.java
+++ Templateexamen23-24/src/main/java/tn/examen/templateexamen2324/controller/ReclamationController.java
@@ -2,6 +2,7 @@ package tn.examen.templateexamen2324.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tn.examen.templateexamen2324.entity.Reclamation;
 import tn.examen.templateexamen2324.entity.TypeReclamation;
@@ -10,6 +11,7 @@ import tn.examen.templateexamen2324.services.IReclamationService;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 @RequestMapping("/reclamation")
 public class ReclamationController {
     @Autowired
@@ -17,9 +19,9 @@ public class ReclamationController {
 
     @GetMapping("/retrieve")
     @ResponseBody
-    public List<Reclamation> getReclamations() {
+    public ResponseEntity<List<Reclamation>> getReclamations() {
         List<Reclamation> reclamationList = iReclamationService.getAllReclamation();
-        return reclamationList;
+        return ResponseEntity.ok().body(reclamationList);
     }
     @PostMapping("/create")
     @ResponseBody
@@ -47,6 +49,7 @@ public class ReclamationController {
     }
     @DeleteMapping("/delete/{id}")
     public void DeleteReclamation(@PathVariable("id") int id){
-        iReclamationService.DeleteReclamation(id);
+        iReclamationService.DeleteReclamation(id
+        );
     }
 }
